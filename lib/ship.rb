d5c21017722442a1933b1d839fc0814e0f7f9e71
Rebecca Appleyard <rklappleyard@gmail.com>
2015-07-28 21:11:53 +0100

    Ship has position when initialized'

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,2 +1,8 @@
 class Ship
+
+  attr_reader :position
+
+  def initialize(opts = {})
+    @position = opts[:position]
+  end
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,5 +1,8 @@
 require 'ship'
 
 describe Ship do
-
+  it "has position when created" do
+    ship = Ship.new(position: 'A1')
+    expect(ship.position).to eq('A1')
+  end
 end
